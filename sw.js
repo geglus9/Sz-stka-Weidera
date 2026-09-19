0d6d88233b41bbb3e0a2e28a375620d5fcdd46d2
geglus9 <geglus9@gmail.com>
2026-09-19 10:05:34 +0200

    Add files via upload
    V2
    
    Zmiana obrazków postaci

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'a6w-v1';
+const CACHE_NAME = 'a6w-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
